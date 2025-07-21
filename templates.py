812038591df6e743b2a341057dfe262e8773ac81
Aadya Banka <kumaraswamy2686@gmail.com>
2025-07-21 13:58:16 +0530

    Project Structure

--- templates.py
+++ templates.py
@@ -38,7 +38,7 @@ for filepath in list_of_files:
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open (filepath, 'w') as f:
             pass
-            logging.info(f"Creating Empty file")
+            logging.info(f"Creating git push origin mainEmpty file")
             
             
     else:
